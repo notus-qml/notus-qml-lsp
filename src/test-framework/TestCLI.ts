31d1576b7aa0fe30c31defd1b408926fd400ebaa
luizh3 <luiz7355608@gmail.com>
2025-06-04 23:24:26 -0300

    feat: Add exit code for unit tests

--- src/test-framework/TestCLI.ts
+++ src/test-framework/TestCLI.ts
@@ -27,8 +27,19 @@ async function run() {
         });
     } catch (exception) {
         console.error(`${exception instanceof Error ? exception.message : exception}`);
+        process.exit(1);
     }
 
 }
+
+process.on('unhandledRejection', (reason) => {
+    console.error('Unhandled Rejection:', reason);
+    process.exit(1);
+});
+
+process.on('uncaughtException', (error) => {
+    console.error('Uncaught Exception:', error);
+    process.exit(1);
+});
 
 run()
