0f20db29c9a6c7bcb5be0709ee270553de98a299
luizh3 <luiz7355608@gmail.com>
2025-08-09 19:25:43 -0300

    feat: Implement functionality that allows implementing rules by regex

--- server/packages/notus-qml-core/src/core/engine/module/ModuleEngine.ts
+++ server/packages/notus-qml-core/src/core/engine/module/ModuleEngine.ts
@@ -1,14 +1,14 @@
-import { ASTNode, LspConfig, LspMethod, ModuleContext, NodeModuleType, Plugin, Rule } from "notus-qml-types";
+import { CodeAnalyzer } from "@/core/utils/CodeAnalyzer";
+import { ASTNode, HandlerType, LspConfig, LspMethod, ModuleContext, Plugin, Rule } from "notus-qml-types";
 
 type ModuleType = Plugin | Rule;
-export type HandlerType = (node: any) => void;
 
 export abstract class ModuleEngine<T extends ModuleType> {
 
     protected cache: Map<string, T>;
-    protected handlersByMethod: Map<LspMethod, Map<NodeModuleType, HandlerType[]>>
+    protected handlersByMethod: Map<LspMethod, Map<string, HandlerType[]>>
     protected context: ModuleContext;
-    protected handlers: Map<NodeModuleType, HandlerType[]> | undefined;
+    protected handlers: Map<string, HandlerType[]> | undefined;
     protected lspConfig: LspConfig | null;
 
     constructor(context: ModuleContext) {
@@ -26,6 +26,7 @@ export abstract class ModuleEngine<T extends ModuleType> {
         this.lspConfig = lspConfig;
     }
 
+    // TODO no make sense receive methodName, always is diagnostics
     setHandlersByMethod(methodName: LspMethod, context: ModuleContext): void {
 
         if (!methodName) {
@@ -48,24 +49,33 @@ export abstract class ModuleEngine<T extends ModuleType> {
 
     }
 
+    runByCode(codeAnalyzer: CodeAnalyzer) {
+
+        const genericSymbol = "_";
+
+        this.handlers?.get(genericSymbol)?.forEach((handler) => {
+            handler(codeAnalyzer)
+        })
+    }
+
     run(node: ASTNode) {
-        this.handlers?.get(node.type as NodeModuleType)?.forEach((handler) => {
+        this.handlers?.get(node.type)?.forEach((handler) => {
             handler(node)
         })
     }
 
-    protected handlersByName(rulesName: string[], context: ModuleContext): Map<NodeModuleType, HandlerType[]> {
+    protected handlersByName(rulesName: string[], context: ModuleContext): Map<string, HandlerType[]> {
 
         const rules = this.getAll(rulesName);
 
-        const rulesByType: Map<NodeModuleType, HandlerType[]> = new Map();
+        const rulesByType: Map<string, HandlerType[]> = new Map();
 
         for (const rule of rules) {
             for (const [name, handlerContext] of Object.entries(rule.handlers)) {
 
                 const handlerImplement = handlerContext.create(context);
 
-                for (const [nodeType, handler] of Object.entries(handlerImplement) as [NodeModuleType, HandlerType][]) {
+                for (const [nodeType, handler] of Object.entries(handlerImplement) as [string, HandlerType][]) {
 
                     if (rulesByType.has(nodeType)) {
                         rulesByType.get(nodeType)!.push(handler);
